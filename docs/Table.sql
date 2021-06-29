081912f2f5dbeac4d85c1138191b747ac6677743
andre2m19 <2180678@my.ipleiria.pt>
2021-06-29 02:16:30 +0100

    Tentativa pagar

--- docs/Table.sql
+++ docs/Table.sql
@@ -33,10 +33,10 @@ CREATE TABLE aeroportos (
   localidade varchar(45) DEFAULT NULL,
   pais varchar(45) DEFAULT NULL,
   telefone varchar(45) DEFAULT NULL,
-  CONSTRAINT pk_idAeroporto PRIMARY KEY (idAeroporto)
+  CONSTRAINT pk_id PRIMARY KEY (id)
 ) ENGINE=InnoDB;
 
-insert into aeroportos(idAeroporto, nome, localidade, pais, telefone)
+insert into aeroportos(id, nome, localidade, pais, telefone)
 values
 (default, 'Aeroporto Luz', 'Lisboa', 'Portugal', '262541983'),
 (default, 'Aeroporto Escuro', 'Madrid', 'Espanha', '262544983'),
@@ -49,38 +49,40 @@ CREATE TABLE voos (
   id int(11) NOT NULL AUTO_INCREMENT,
   descricao varchar(100) DEFAULT NULL,
   preco decimal(7,2) DEFAULT NULL,
-  idAeroOrigem int(11) DEFAULT NULL,
-  idAeroDestino int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY fk_voos_idAeroOrigem (idAeroOrigem),
-  KEY fk_voos_idAeroDestino (idAeroDestino),
-  CONSTRAINT fk_voos_idAeroOrigem FOREIGN KEY (idAeroOrigem) REFERENCES aeroportos (idAeroporto),
-  CONSTRAINT fk_voos_idAeroDestino FOREIGN KEY (idAeroDestino) REFERENCES aeroportos (idAeroporto)
+  idaeroportoorigem int(11) DEFAULT NULL,
+  idaeroportodestino int(11) DEFAULT NULL,
+  PRIMARY KEY (id),
+  KEY fk_voos_idAeroportoOrigem (idaeroportoorigem),
+  KEY fk_voos_idAeroportoDestino (idaeroportodestino),
+  CONSTRAINT fk_voos_idaeroportoorigem FOREIGN KEY (idaeroportoorigem) REFERENCES aeroportos (id),
+  CONSTRAINT fk_voos_idaeroportodestino FOREIGN KEY (idaeroportodestino) REFERENCES aeroportos (id)
 ) ENGINE=InnoDB;
 
-insert into voos(id, descricao, preco, idAeroOrigem, idAeroDestino)
+
+insert into voos(id, descricao, preco, idaeroportoorigem, idaeroportodestino)
 values
-(1,'Lisboa-Paris',265.00,1,2),
-(2,'Londres-Dubai',679.00,1,4);
+(1,'Lisboa-Madrid',265.00,1,2),
+(2,'Londres-Dubai',679.00,3,4);
 
 drop table escalas;
 CREATE TABLE escalas (
   idEscala int(11) NOT NULL AUTO_INCREMENT,
-  idAeroOrigem int(11) DEFAULT NULL,
-  idAeroDestino int(11) DEFAULT NULL,
+  idaeroportoorigem int(11) DEFAULT NULL,
+  idaeroportodestino int(11) DEFAULT NULL,
   idVoo int(11) DEFAULT NULL,
   distancia double NOT NULL,
-  dOrigem DATE DEFAULT NULL,
-  dDestino DATE DEFAULT NULL,
+  dataorigem date NOT NULL,
+  datadestino DATE NOT NULL,
   CONSTRAINT pk_idEscala PRIMARY KEY(idEscala),
-  CONSTRAINT fk_escalas_idAeroOrigem FOREIGN KEY (idAeroOrigem) references aeroportos(idAeroporto),
-  CONSTRAINT fk_escalas_idAeroDestino FOREIGN KEY (idAeroDestino) references aeroportos(idAeroporto),
+  CONSTRAINT fk_escalas_idaeroportoorigem FOREIGN KEY (idaeroportoorigem) references aeroportos(id),
+  CONSTRAINT fk_escalas_idaeroportodestino FOREIGN KEY (idaeroportodestino) references aeroportos(id),
   CONSTRAINT fk_escalas_idVoo FOREIGN KEY (idVoo) references voos(id)
 ) ENGINE = InnoDB;
 
-insert into escalas(idEscala, idAeroOrigem, idAeroDestino, idVoo, distancia, dOrigem, dDestino)
+insert into escalas(idEscala, idaeroportoorigem, idaeroportodestino, idVoo, distancia, dataorigem, datadestino)
 values
-(1, 1, 2, 1, 1382,'1980-12-17', '1990-12-17');
+(1, 1, 2, 1, 1382, 1980-12-17, 1990-12-17),
+(2, 3, 4, 2, 5076, 2000-12-17, 210-12-17);
 
 CREATE TABLE aviao (
   idAviao  int(11) NOT NULL AUTO_INCREMENT,
