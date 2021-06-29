cc9be208d8b92300907fd2d558a2ac3fdb1ba69e
Simonegomes0 <2201092@my.ipleiria.pt>
2021-06-29 02:18:22 +0100

    base de dados

--- docs/Table.sql
+++ docs/Table.sql
@@ -1,13 +1,13 @@
 Create database if not exists FlighTravelAir;
 Use FlighTravelAir;
-drop database flightravelair;
 
-DROP TABLE IF EXISTS users;
+
+DROP TABLE IF EXISTS `users`;
 DROP TABLE IF EXISTS `aeroportos`; --
 DROP TABLE IF EXISTS `escalaaviao`;
 DROP TABLE IF EXISTS `escalas`; --
 DROP TABLE IF EXISTS `passagemvenda`;
-DROP TABLE IF EXISTS `planes`; --
+DROP TABLE IF EXISTS `aviao`; --
 DROP TABLE IF EXISTS `users`; -- 
 DROP TABLE IF EXISTS `voos`; --
 
@@ -26,9 +26,9 @@ CREATE TABLE users (
   CONSTRAINT uk_users_nif UNIQUE KEY (nif)
 ) ENGINE=InnoDB;
 
-drop table aeroportos;
+
 CREATE TABLE aeroportos (
-  id int(11) NOT NULL AUTO_INCREMENT,
+  idAeroporto int(11) NOT NULL AUTO_INCREMENT,
   nome varchar(45) DEFAULT NULL,
   localidade varchar(45) DEFAULT NULL,
   pais varchar(45) DEFAULT NULL,
@@ -44,7 +44,7 @@ values
 (default, 'Dubai Internacional Airport', 'Dubai', 'Arabes United', '787432564');
 
 
-drop table voos;
+
 CREATE TABLE voos (
   id int(11) NOT NULL AUTO_INCREMENT,
   descricao varchar(100) DEFAULT NULL,
@@ -63,7 +63,7 @@ values
 (1,'Lisboa-Paris',265.00,1,2),
 (2,'Londres-Dubai',679.00,1,4);
 
-drop table escalas;
+
 CREATE TABLE escalas (
   idEscala int(11) NOT NULL AUTO_INCREMENT,
   idAeroOrigem int(11) DEFAULT NULL,
@@ -89,4 +89,6 @@ CREATE TABLE aviao (
   tipoAviao varchar(45) DEFAULT NULL,
   CONSTRAINT pk_idAviao PRIMARY KEY (idAviao)
 ) ENGINE = InnoDB;
+
+
 
