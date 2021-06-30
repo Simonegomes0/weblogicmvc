b0ef7a2ad1561e4f0e1fc300a6f725b74609c5d1
Simonegomes0 <2201092@my.ipleiria.pt>
2021-06-30 02:05:38 +0100

    update

--- webapp/router.php
+++ webapp/router.php
@@ -6,34 +6,28 @@
  * Time: 11:18
  */
 use ArmoredCore\Facades\Router;
-
 /****************************************************************************
  *  URLEncoder/HTTPRouter Routing Rules
  *  Use convention: controllerName/methodActionName
  ****************************************************************************/
-
-Router::get('/',			'HomeController/index');
-Router::get('home/',		'HomeController/index');
-Router::get('home/index',	'HomeController/index');
-Router::get('home/start',	'HomeController/start');
-
+Router::get('/',         'HomeController/index');
+Router::get('home/',      'HomeController/index');
+Router::get('home/index',  'HomeController/index');
+Router::get('home/start',  'HomeController/start');
 // Login
-Router::get('home/login',	'LoginController/doLogin');
+Router::get('home/login',  'LoginController/doLogin');
 Router::get('login/login',          'LoginController/getLoginForm');
 Router::get('login/registration',   'LoginController/getRegistrationForm');
 Router::get('login/logout',         'LoginController/destroySession');
 Router::post('login/doregistration', 'LoginController/doRegistration');
 Router::post('login/dologin',       'LoginController/doLogin');
-
 // Cliente
-
 Router::get('cliente/bilhete', 'ClienteController/VerVoos');
 Router::post('cliente/bilhete', 'ClienteController/VerVoos');
 Router::get('cliente/index', 'ClienteController/index');
 Router::get('cliente/comprar', 'ClienteController/Comprar');
 Router::post('cliente/pagar', 'ClienteController/Pagar');
 Router::get('cliente/mostrar', 'ClienteController/Mostrar');
-
 //Admin
 Router::get('admin/index', 'AdminController/index');
 Router::get('admin/GestaoAero', 'AdminController/GestaoAero');
@@ -43,7 +37,6 @@ Router::get('admin/doUpdateAero',   'AdminController/doUpdateAero');
 Router::post('admin/doUpdateAero',   'AdminController/doUpdateAero');
 Router::get('admin/aeroUpdate',   'AdminController/aeroUpdate');
 Router::get('admin/AeroEliminar',   'AdminController/AeroEliminar');
-
 Router::get('admin/Funcionario', 'AdminController/Funcionarios');
 Router::get('admin/funcioUpdate', 'AdminController/funcioUpdate');
 Router::get('admin/doUpdateFuncio', 'AdminController/doUpdateFuncio');
@@ -53,17 +46,10 @@ Router::post('admin/funcioAdd', 'AdminController/funcioAdd');
 Router::get('admin/funcioAdd', 'AdminController/funcioAdd');
 Router::get('admin/funcioEliminar', 'AdminController/funcioEliminar');
 
-
 //GestorVoo
-
 Router::get('gestorvoo/index', 'GestorVooController/index');
-
 //OpCheckIn
-
 Router::get('opcheckin/index', 'OpCheckInController/index');
-
-
-
 
 
 
